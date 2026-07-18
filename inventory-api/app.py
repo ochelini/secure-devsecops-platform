a643df85eba3617aab634c45ab6d568ddef3a8d8
ochelini <ochelini@gmail.com>
2026-07-18 14:33:16 -0300

    Initial microservices platform with Docker Compose

---
+++ inventory-api/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+PRODUCTS = [
+    {"id": 1, "name": "Tomatoes"},
+    {"id": 2, "name": "Chicken"},
+    {"id": 3, "name": "Rice"}
+]
+
+@app.route("/")
+def home():
+    return "Inventory API Running"
+
+@app.route("/products")
+def products():
+    return jsonify(PRODUCTS)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ worker/worker.py
@@ -0,0 +1,5 @@
+import time
+
+while True:
+    print("Worker processing inventory jobs...")
+    time.sleep(30)
